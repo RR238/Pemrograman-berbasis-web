95644a1f8d0de5ffd2ca7c0cb7d82b705055f86d
Rafid Faiz <raizzzptr@gmail.com>
2025-03-24 06:06:35 +0700

    Delete Tugas directory

--- Tugas/Tugas3/script.js
+++
@@ -1,8 +0,0 @@
-const generateFibonacci = () => {
-    let n = document.getElementById("fibonacciInput").value;
-    let fib = [0, 1];
-    for (let i = 2; i < n; i++) {
-        fib.push(fib[i - 1] + fib[i - 2]);
-    }
-    document.getElementById("fibonacciOutput").innerText = fib.slice(0, n).join(', ');
-};
